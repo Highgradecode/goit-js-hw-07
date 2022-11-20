5181169a15517a7517769dad65aa1903cf04df66
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-11-20 17:05:40 +0200

    02-gallery-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryLayout = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+}).join('')
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryLayout);
+
+
+const lightboxOptions = {
+    captionsData: 'alt',
+    captionDelay: 250,
+}
+
+var lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
+
+
+
